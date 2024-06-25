8a94496c2bbcffe3df93e4f6f7d273b74a8e14b5
JackeyZhou1 <103830209+JackeyZhou1@users.noreply.github.com>
2024-06-25 09:46:37 +0800

    Delete chat2db-server directory

--- chat2db-server/chat2db-plugins/chat2db-oceanbase/src/main/java/ai/chat2db/plugin/oceanbase/OceanBaseDBManage.java
+++
@@ -1,8 +0,0 @@
-package ai.chat2db.plugin.oceanbase;
-
-import ai.chat2db.spi.DBManage;
-import ai.chat2db.spi.jdbc.DefaultDBManage;
-
-public class OceanBaseDBManage extends DefaultDBManage implements DBManage {
-
-}
--- chat2db-server/chat2db-plugins/chat2db-oceanbase/src/main/java/ai/chat2db/plugin/oceanbase/OceanBaseMetaData.java
+++
@@ -1,9 +0,0 @@
-package ai.chat2db.plugin.oceanbase;
-
-import ai.chat2db.spi.MetaData;
-import ai.chat2db.spi.jdbc.DefaultMetaService;
-
-public class OceanBaseMetaData extends DefaultMetaService implements MetaData {
-
-
-}
